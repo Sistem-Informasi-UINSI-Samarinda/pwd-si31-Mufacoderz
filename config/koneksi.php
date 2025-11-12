9bfc1d8ceba00dc4e9364fc97a6f1ab42a40c191
Muhammad <mf9555759@gmail.com>
2025-11-12 14:59:46 +0800

    praktikum1

---
+++ config/koneksi.php
@@ -0,0 +1,18 @@
+<?php 
+
+$servername = "localhost";
+$database = "pwd_2441919016";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn){
+    die("Koneksi gagal: ". mysqli_connect_error());
+};
+
+echo "Koneksi berhasil";
+mysqli_close($conn);
+
+
+?>
